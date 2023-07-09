c8e7ca884a6d80894dc88e0af095bbf3cca71a83
Micael006 <micael_micael1@mial.ru>
2023-07-09 11:11:54 +0300

    level 1

--- Scripts/KnifeSpawner.gd
+++ Scripts/KnifeSpawner.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
-@export var knife_scene: PackedScene = preload("res://Prefabs/knife.tscn")
+# Префабы ножей
+@export var knives : Array[PackedScene]
+# Ограничение угла, с которого можно запустить нож  [0, 2PI]
+@export var positions : Array[Vector2]
+# Типы запускаемых ножей
+@export var types : Array[int]
+#@export var knife_scene: PackedScene = preload("res://Prefabs/knife.tscn")
 @export var warning_scene: PackedScene = preload("res://Prefabs/warning.tscn")
 @export var distance: float = 600.0
 
+@onready var cur_index : int
+
+signal victory
+
 func new_game():
 	$MobTimer.start()
+	cur_index = 0
 
 func _ready():
 	new_game()
@@ -19,13 +30,17 @@ func _on_mob_timer_timeout():
 	spawn_on_circle()
 
 func spawn_on_circle():
-	var rand: float = randf()*2*PI
+	var rand: float = (randf() * (positions[cur_index].y - positions[cur_index].x) + positions[cur_index].x) * PI
 	var dir: Vector2 = Vector2(cos(rand), sin(rand))
-	var attack : Node2D = knife_scene.instantiate()
+	var attack : Node2D = knives[types[cur_index]].instantiate()
 	var warning : Node2D = warning_scene.instantiate()
 	attack.position = dir * distance
 	attack.rotation = rand - PI * 0.5
 	add_child(attack)
 	warning.max_dist = distance
 	attack.add_child(warning)
+	if(cur_index == positions.size() - 1):
+		$MobTimer.stop()
+		emit_signal("victory")
+	cur_index += 1
 	
--- Scripts/knife.gd
+++ Scripts/knife.gd
@@ -1,11 +1,17 @@
 extends Sprite2D
 
 @export var speed : float = 150.0
+@export var the_chosen_one : bool = false
 
 var velocity : Vector2 = Vector2()
 
 func _ready():
-	velocity = Vector2.UP.rotated(self.rotation) * speed
+	if(the_chosen_one):
+		$DamageComponent.damage = 0
+		$DamageComponent.set_deferred("monitoring", false)
+		velocity = Vector2.ZERO
+	else:
+		velocity = Vector2.UP.rotated(self.rotation) * speed
 
 
 func _process(delta):
@@ -13,12 +19,13 @@ func _process(delta):
 
 func _on_damagebox_entered(area):
 	if area is HitboxComponent : 
-		call_deferred("rebase",area)
+		call_deferred("rebase", area)
 	elif area is DamageboxComponent:
-		queue_free()
+		if($DamageboxComponent.monitoring):
+			queue_free()
 
 func rebase(area : Node2D):
-	$DamageboxComponent.set_deferred("monitoring",false)
+	$DamageboxComponent.set_deferred("monitoring", false)
 	set_process(false)
 	var parent = area.get_parent()
 	reparent(parent,true)
